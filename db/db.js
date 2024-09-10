f4028c91b78b16a88838e5c104c57634192c7ef3
juaniB12 <bjuan.ignacio11@gmail.com>
2024-09-10 10:19:01 -0300

    changes

--- db/db.js
+++ db/db.js
@@ -1,6 +1,14 @@
 const mysql = require("mysql2");
 const config = require("./config");
 
-const conn = mysql.createConnection(config);
+const conn = mysql.createConnection({
+    host: process.env.MDB_HOST,
+    port: process.env.MDB_PORT,
+    user: process.env.MDB_USER,
+    password: process.env.MDB_PASS, 
+    database: process.env.MDB_DB, 
+    keepAliveInitialDelay: 10000, // 0 by default.
+    enableKeepAlive: true, // false by default.
+});
 
 module.exports = conn;
