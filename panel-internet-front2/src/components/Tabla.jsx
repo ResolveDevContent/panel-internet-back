830f9675cda3ea5097b93df88f12841442e0c934
juaniB12 <bjuan.ignacio11@gmail.com>
2024-09-26 20:29:57 -0300

    changes

--- panel-internet-front2/src/components/Tabla.jsx
+++ panel-internet-front2/src/components/Tabla.jsx
@@ -37,7 +37,8 @@ const formatearDatos = (datos) => {
     const columns = Object.keys(datos[0]).slice(1, Object.keys(datos[0]).length)
 
     const rows = datos.map((data) => {
-        if((titulo == 'transacciones' || titulo == 'comercios/pagos') && !data["fecha"].includes("/")) {
+        if((titulo == 'historial' || titulo == 'transacciones' || titulo == 'comercios/pagos') && !data["fecha"].includes("/")) {
+            console.log("FECHA A LA VUELTA",Number(data['fecha']))
             let date = new Date(Number(data['fecha']));
             const fecha = date.toISOString();
             const hora = (fecha.split('T')[1]).split(':');
