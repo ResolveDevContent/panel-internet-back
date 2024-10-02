f03c6fd4d7b579235bd1c7ae2b616578a32fe4a3
juaniB12 <bjuan.ignacio11@gmail.com>
2024-10-02 16:11:59 -0300

    changes

--- routes/abm.js
+++ routes/abm.js
@@ -910,7 +910,7 @@ router.delete("/admins/borrar/:id", authenticate, async (req, res) => {
             const permisosborrar = await deleteRecord("permisos", "ID_Admin", admins[0].email);
         }
         
-        const users = await deleteRecord("users", "userId", id);
+        const users = await deleteRecord("users", "email", admins[0].email);
         const result = await deleteRecord("admins", "ID_Admin", id);
         await insertRecord('historial', {message: "Se borro el admin " + admins[0].nombre, fecha: Date.now()});
         res.status(200).json(result);
