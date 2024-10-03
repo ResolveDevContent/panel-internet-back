12194da9d89929caeb72980348b2051b1a90c6ad
juaniB12 <bjuan.ignacio11@gmail.com>
2024-10-03 09:34:13 -0300

    changes

--- routes/abm.js
+++ routes/abm.js
@@ -863,40 +863,42 @@ router.post("/admins/agregar", authenticate, async (req, res) => {
 });
 
 router.put("/admins/modificar/:id", authenticate, async (req, res) => {
-    console.log(req.body)
-    // const { id } = req.params;
-    // const admin = {
-    //     nombre: req.body.nombre,
-    //     apellido: req.body.apellido,
-    //     email: req.body.email,
-    //     permisos: req.body.permisos
-    // }
+    const { id } = req.params;
+    const admin = {
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email,
+        permisos: req.body.permisos
+    }
 
     
-    // try {
-    //     const updateAdmin = await updateRecord("admins", admin, "ID_Admin", id);
+    try {
+        const updateAdmin = await updateRecord("admins", admin, "ID_Admin", id);
         
-    //     if(req.body.password) {
-    //         const salt = await bcrypt.genSalt(10);
-    //         const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        if(req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
         
-    //         const newPassword = {
-    //             password: hashedPassword
-    //         }
+            const newPassword = {
+                password: hashedPassword
+            }
             
-    //         const updateUser = await updateRecord("users", newPassword, "email", req.body.email);
-    //     }
+            const updateUser = await updateRecord("users", newPassword, "email", req.body.email);
+        }
 
-    //     const permisos = {
+        for(const comercio of req.body.ID_Comercio) {
+            const objComercio = {
+                ID_Comercio: comercio
+            }
 
-    //     }
-    //     const updatePermisos = await updateRecord("permisos", newPassword, "email", req.body.email);
+            const updatePermisos = await updateRecord("permisos", objComercio, "ID_Admin", req.body.email);
+        }
 
-    //     await insertRecord('historial', {message: "Se actualizo el admin " + req.body.nombre, fecha: Date.now()});
-    //     res.status(200).json(results);
-    // } catch (err) {
-    //     res.status(500).json({ error: "Se ha producido un error, inténtelo nuevamente." });
-    // }
+        await insertRecord('historial', {message: "Se actualizo el admin " + req.body.nombre, fecha: Date.now()});
+        res.status(200).json(results);
+    } catch (err) {
+        res.status(500).json({ error: "Se ha producido un error, inténtelo nuevamente." });
+    }
 });
 
 // Borrar administrador
