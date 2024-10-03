355708466ee04b3489c4293de014eb70e9dd4e8a
juaniB12 <bjuan.ignacio11@gmail.com>
2024-10-03 10:54:00 -0300

    Changes

--- routes/abm.js
+++ routes/abm.js
@@ -965,6 +965,7 @@ router.post("/puntos/fecha/agregar", authenticate, async (req, res) => {
 
     if(req.body.fecha <= Date.now()) {
         const masage = caducarPuntos(req.body.fecha);
+        console.log(masage)
         return res.status(201).json(masage);
     }
 
