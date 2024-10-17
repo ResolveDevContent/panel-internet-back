ec77676ee01945653b241a5bb61e450e279ece10
juaniB12 <bjuan.ignacio11@gmail.com>
2024-10-17 11:35:12 -0300

    changse

--- panel-internet-front2/src/templates/UserTemplate.jsx
+++ panel-internet-front2/src/templates/UserTemplate.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export const UserTemplate = ({user}) => { 
+export const UserTemplate = ({user = {}, values = []}) => { 
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
@@ -8,7 +8,7 @@ export const UserTemplate = ({user}) => {
             setLoading(true)
         }
 
-    }, [])
+    }, [values])
 
     return loading ? (
         <li>
