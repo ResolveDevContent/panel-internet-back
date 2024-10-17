b6239871dbd3ea1a59b629a068149d01f7b7b28d
juaniB12 <bjuan.ignacio11@gmail.com>
2024-10-17 11:49:44 -0300

    changse

--- panel-internet-front2/src/templates/UserTemplate.jsx
+++ panel-internet-front2/src/templates/UserTemplate.jsx
@@ -1,14 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { PerfilAuth } from '../services/auth'
+
 
 export const UserTemplate = ({user = {}}) => { 
+    const [userObj, setUserObj] = useState({});
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        console.log(user)
+        if(!user) {
+            setLoading(true)
+            PerfilAuth().then((result) => {
+                console.log(result)
+                setUserObj(result.message)
+            }).finally((res) => {
+                setLoading(false)
+            })
+        }
     }, [user])
 
-    return (
+    return loading ? null : (
         <li>
-            <input type="hidden" id="user" name="user" value={JSON.stringify(user)}/>
+            <input type="hidden" id="user" name="user" value={JSON.stringify(user ? user : userObj)}/>
         </li>
     )
 }
