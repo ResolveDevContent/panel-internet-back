dfefc412ec6ffee69aaf2d53ad035e0831415658
juaniB12 <bjuan.ignacio11@gmail.com>
2024-10-17 11:41:47 -0300

    changse

--- panel-internet-front2/src/App.jsx
+++ panel-internet-front2/src/App.jsx
@@ -50,45 +50,45 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/login/cliente" element={<LoginClienteForm />} />
             <Route path="/" element={
-              <Panel user={user}/>
+              <Panel user={user ? user : null}/>
             }>
-              <Route element={<PrivateRoutes user={user} roles={['superadmin']}/>}>
+              <Route element={<PrivateRoutes user={user ? user : null} roles={['superadmin']}/>}>
                 <Route path="backups" element={<Backups titulo={"backups"}/>} />
 
                 <Route path="puntos/fecha" element={<PuntosFecha titulo={"puntos/fecha"}/>} />
-                <Route path="historial/listar" element={<Listado user={user} titulo={"historial"}/>} />
+                <Route path="historial/listar" element={<Listado user={user ? user : null} titulo={"historial"}/>} />
                 <Route path="historial/ver/:id" element={<Ver elementos={ELEMENTOS.historial} titulo={"historial"}/>} />
                 <Route path="cambiarcontraseña" element={<ChangePassword titulo={"users"} />} />
                 <Route path="clientes/importarcsv" element={<ImportCsv titulo={"clientes"} />} />
-                <Route path="clientes/agregar" element={<Form elementos={ELEMENTOS.clientes} user={user} titulo={"clientes"} />} />
-                <Route path="clientes/editar/:id" element={<Form elementos={ELEMENTOS.clientes} titulo={'clientes'}/>} user={user}/>
-                <Route path="admins/listar" element={<Listado user={user} titulo={"admins"}/>} />
-                <Route path="admins/agregar" element={<Form elementos={ELEMENTOS.admins} user={user} titulo={'admins'}/>} />
-                <Route path="admins/editar/:id" element={<Form elementos={ELEMENTOS.admins} titulo={'admins'} user={user}/>} />
+                <Route path="clientes/agregar" element={<Form elementos={ELEMENTOS.clientes} user={user ? user : null} titulo={"clientes"} />} />
+                <Route path="clientes/editar/:id" element={<Form elementos={ELEMENTOS.clientes} titulo={'clientes'}/>} user={user ? user : null}/>
+                <Route path="admins/listar" element={<Listado user={user ? user : null} titulo={"admins"}/>} />
+                <Route path="admins/agregar" element={<Form elementos={ELEMENTOS.admins} user={user ? user : null} titulo={'admins'}/>} />
+                <Route path="admins/editar/:id" element={<Form elementos={ELEMENTOS.admins} titulo={'admins'} user={user ? user : null}/>} />
                 <Route path="comercios/agregar" element={<Form elementos={ELEMENTOS.comercios} titulo={'comercios'}/>} />
                 <Route path="comercios/editar/:id" element={<Form elementos={ELEMENTOS.comercios} titulo={'comercios'}/>} />
               </Route>
-              <Route element={<PrivateRoutes user={user} roles={['admin', 'superadmin']}/>}>
-                <Route path="clientes/listar" element={<Listado user={user} titulo={"clientes"}/>} />
-                <Route path="comercios/listar" element={<Listado user={user} titulo={"comercios"}/>} />
-                <Route path="comercios/pagos/listar" element={<Listado user={user} titulo={'comercios/pagos'}/>} />
-                <Route path="comercios/pagos/agregar" element={<Form elementos={ELEMENTOS.pagos} user={user} titulo={'comercios/pagos'}/> } />
+              <Route element={<PrivateRoutes user={user ? user : null} roles={['admin', 'superadmin']}/>}>
+                <Route path="clientes/listar" element={<Listado user={user ? user : null} titulo={"clientes"}/>} />
+                <Route path="comercios/listar" element={<Listado user={user ? user : null} titulo={"comercios"}/>} />
+                <Route path="comercios/pagos/listar" element={<Listado user={user ? user : null} titulo={'comercios/pagos'}/>} />
+                <Route path="comercios/pagos/agregar" element={<Form elementos={ELEMENTOS.pagos} user={user ? user : null} titulo={'comercios/pagos'}/> } />
                 <Route path="comercios/pagos/editar/:id" element={<Form elementos={ELEMENTOS.pagos} titulo={'comercios/pagos'}/>} />
-                <Route path="asociaciones/listar" element={<Listado user={user} titulo={"asociaciones"}/>} />
+                <Route path="asociaciones/listar" element={<Listado user={user ? user : null} titulo={"asociaciones"}/>} />
                 <Route path="asociaciones/agregarclientes/agregar" element={<Form elementos={ELEMENTOS.asociacionesClientes} titulo={'asociaciones/clientes'} user={user}/>} />
                 <Route path="asociaciones/agregarcomercios/agregar" element={<Form elementos={ELEMENTOS.asociacionesComercios} titulo={'asociaciones/comercios'} user={user}/>} />
               </Route>
               {!loading ? (
-                <Route element={<PrivateRoutes user={user} roles={['admin', 'comercio', 'superadmin']}/>}>
-                  <Route path="transacciones/listar" element={<Listado user={user} titulo={"transacciones"}/>} />
-                  <Route path="transacciones/agregar" element={<Form elementos={ELEMENTOS.transacciones} titulo={'transacciones'} user={user} />}  />
-                  <Route path="transacciones/editar/:id" element={<Form elementos={ELEMENTOS.transacciones} titulo={'transacciones'}/>} user={user} />
+                <Route element={<PrivateRoutes user={user ? user : null} roles={['admin', 'comercio', 'superadmin']}/>}>
+                  <Route path="transacciones/listar" element={<Listado user={user ? user : null} titulo={"transacciones"}/>} />
+                  <Route path="transacciones/agregar" element={<Form elementos={ELEMENTOS.transacciones} titulo={'transacciones'} user={user ? user : null} />}  />
+                  <Route path="transacciones/editar/:id" element={<Form elementos={ELEMENTOS.transacciones} titulo={'transacciones'}/>} user={user ? user : null} />
                 </Route>
               ) : null}
               {!loading ? (
-                <Route element={<PrivateRoutes user={user} roles={['cliente']}/>}>
+                <Route element={<PrivateRoutes user={user ? user : null} roles={['cliente']}/>}>
                   <Route path='cliente' element={<User />} />
-                  <Route path='historial/transacciones/listar' element={<Listado user={user} titulo={'historial/transacciones'} />} />
+                  <Route path='historial/transacciones/listar' element={<Listado user={user ? user : null} titulo={'historial/transacciones'} />} />
                 </Route>
               ) : null}
             </Route>
