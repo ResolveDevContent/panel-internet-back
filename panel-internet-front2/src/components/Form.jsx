1bdb9c4fce8cfaaca8c193a0824c337fd6c0f6f3
juaniB12 <bjuan.ignacio11@gmail.com>
2024-10-21 18:56:37 -0300

    changes

--- panel-internet-front2/src/components/Form.jsx
+++ panel-internet-front2/src/components/Form.jsx
@@ -41,7 +41,7 @@ export const Form = ({elementos = [], titulo = '', user = {}}) => {
 
             if((titulo == "admins" && name == "ID_Comercio") ||
             titulo == "asociaciones/clientes" ||
-            (titulo == "asociaciones/comercios" && name == "ID_Comercio") ||
+            (titulo == "asociaciones/comercios" && name == "ID_Comercio") || (titulo == "asociaciones/comercios" && name == "ID_Cliente") ||
             (titulo == "comercios/pagos" && name == "ID_Comercio")) { 
                 value = JSON.parse(value)    
             }
