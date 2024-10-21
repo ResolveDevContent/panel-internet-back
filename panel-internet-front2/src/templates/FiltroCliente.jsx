093df7fa07b970604e057c8aae2120dcff53507e
juaniB12 <bjuan.ignacio11@gmail.com>
2024-10-21 14:56:52 -0300

    changes

--- panel-internet-front2/src/templates/FiltroCliente.jsx
+++ panel-internet-front2/src/templates/FiltroCliente.jsx
@@ -178,7 +178,7 @@ export const FiltroCliente = ({data, titulo, values = {}, user = {}}) => {
                             : <>
                                 <ul>
                                 {otherOpciones.map((row, idx) => (
-                                    <li key={idx} onClick={buscarCliente(row.ID_Cliente)}>
+                                    <li key={idx} onClick={()  => buscarCliente(row.ID_Cliente)}>
                                         <label>
                                             <input type='radio' id={row.ID_Cliente} value={row.ID_Cliente} name="listCliente" onChange={e => setOtherDato(e.target.value)}/>
                                             <span>{row.nombre}</span>
