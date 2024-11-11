377eaaf711777c566f1ca6c91ed4cca2c38dcaf2
juaniB12 <bjuan.ignacio11@gmail.com>
2024-11-11 16:37:51 -0300

    changes

--- panel-internet-front2/src/services/Mikrowisp.js
+++ panel-internet-front2/src/services/Mikrowisp.js
@@ -3,7 +3,6 @@ export const listarFacturas = (body) => {
     return (
         fetch(`https://autogestion.winetinternet.com.ar/api/v1/GetInvoices`, {
             method: "POST",
-            mode: "no-cors",
             headers: {
                 "Access-Control-Allow-Headers" : "Content-Type",
                 "Access-Control-Allow-Origin": "*",
