1ee2efccdd6c230862b27782244432ea46460b62
juaniB12 <bjuan.ignacio11@gmail.com>
2024-11-11 10:16:48 -0300

    Changes

--- panel-internet-front2/src/services/Mikrowisp.js
+++ panel-internet-front2/src/services/Mikrowisp.js
@@ -9,7 +9,7 @@ export const listarFacturas = (body) => {
             },
             body: JSON.stringify(body)
         })
-        .then((res) => res.json())
+        .then((res) => res.text())
         .catch((err) => console.log(err))
     )
 }
