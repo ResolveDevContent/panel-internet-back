9106ee09d8ec9165b45ee50bf22b3452e9b2e962
juaniB12 <bjuan.ignacio11@gmail.com>
2024-12-19 13:32:30 -0300

    changes

--- routes/abm.js
+++ routes/abm.js
@@ -657,14 +657,12 @@ async function agregarClientes(datos) {
         }
     });
     const resultados = await Promise.all(promises);
-    console.log(datos[0]);
     const clientesParaAgregar = datos.filter((cliente, index) => resultados[index] === "agregar");
     const clientesParaActualizar = datos.filter((cliente, index) => resultados[index] === "actualizar");
 
     const clientesAgregar = clientesParaAgregar.map((cliente) => [cliente.Id,cliente.Codigo,cliente.dni,cliente.nombre,cliente.apellido,cliente.direccion_principal,cliente.email,cliente.zona,cliente.activo]);
     try {
         if(clientesParaAgregar.length > 0) {
-            console.count("agregar")
             await batchInsert("clientes", clientesParaAgregar, clientesAgregar);
         }
 
