5f863b4320dfff533075b9f9b6650f97c9508df2
juaniB12 <bjuan.ignacio11@gmail.com>
2024-12-19 12:47:52 -0300

    changes

--- routes/abm.js
+++ routes/abm.js
@@ -646,7 +646,7 @@ async function agregarClientes(datos) {
 
             const findClient = clientes.find((doc) => doc.Id == row.Id && doc.Codigo == row.Codigo)
 
-            return findClient ? "actualizar" : "agregar"
+            return Object.keys(findClient).length > 0 ? "actualizar" : "agregar"
         } else {
             return "false";
         }
