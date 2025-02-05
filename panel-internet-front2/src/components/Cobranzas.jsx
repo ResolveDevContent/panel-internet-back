ce4fe67fa1c767037f972a83b8bab36f5cb016bf
LucasMezza <mezza.lucas12@gmail.com>
2025-02-05 15:31:15 -0300

    cobranzas

--- panel-internet-front2/src/components/Cobranzas.jsx
+++ panel-internet-front2/src/components/Cobranzas.jsx
@@ -106,8 +106,8 @@ export const Cobranzas = ({user = {}}) => {
 
         dataObj.user = user;
 
-        console.log(totalFacturas, dataObj, clienteObj)
-        if(totalFacturas.arr.length > 0 && (Number(dataObj.monto_total) > 0 || Number(dataObj.puntos_pago) == Number(dataObj.monto_total)) &&
+        console.log(totalFacturas, dataObj, clienteObj, totalFacturasRef.current)
+        if(totalFacturas.arr.length > 0 && (Number(dataObj.monto_total) > 0 || Number(dataObj.puntos_pago) == Number(totalFacturasRef.current)) &&
             (Number(dataObj.puntos_pago) == 0 || Number(dataObj.puntos_pago) <= Number(clienteObj.puntos))) {
             setLoading(true)
 
